e3680c5a16789968f54ae4aa1f746a83946089e5
KevinBTSsio <kev17.haramendy@gmail.com>
2019-01-24 17:00:40 +0100

    Suite

--- app/src/main/java/com/example/maximeglod/fbta/MainActivity.java
+++ app/src/main/java/com/example/maximeglod/fbta/MainActivity.java
@@ -1,19 +1,27 @@
 package com.example.maximeglod.fbta;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
     TextView tv_poids, tv_taille, tv_nom, tv_age, tv_sexe, tv_sport, tv_objectif, tv_prenom;
-    EditText et_poids, et_taille, et_nom, et_age, et_sexe, et_sport, et_objectif, et_prenom;
+    EditText et_poids, et_taille, et_nom, et_age, et_objectif, et_prenom;
     Button bt_valider;
+    CheckBox cb_sexe1, cb_sexe2, cb_sexe3;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,21 +35,42 @@ public class MainActivity extends AppCompatActivity {
         tv_taille = (TextView) findViewById(R.id.tvTaille);
         tv_sexe = (TextView) findViewById(R.id.tvSexe);
         tv_sport = (TextView) findViewById(R.id.tvSport);
+
         et_age = (EditText) findViewById(R.id.etAge);
         et_nom = (EditText) findViewById(R.id.etNom);
         et_objectif = (EditText) findViewById(R.id.etObjectif);
         et_poids = (EditText) findViewById(R.id.etPoids);
         et_prenom = (EditText) findViewById(R.id.etPrenom);
         et_taille = (EditText) findViewById(R.id.etTaille);
-        et_sexe = (EditText) findViewById(R.id.etSexe);
-        et_sport = (EditText) findViewById(R.id.etActivite);
+
+        cb_sexe1 = (CheckBox) findViewById(R.id.cbSexe1);
+        cb_sexe2 = (CheckBox) findViewById(R.id.cbSexe2);
+        cb_sexe3 = (CheckBox) findViewById(R.id.cbSexe3);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        List liste = new ArrayList();
+        liste.add("Détente");
+        liste.add("Modéré");
+        liste.add("Intense");
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,liste);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
         bt_valider = (Button) findViewById(R.id.btnProfil);
 
         bt_valider.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), tv_prenom+" "+tv_nom+" enregistré", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), et_prenom.getText().toString() + " " + et_nom.getText().toString() + " enregistré", Toast.LENGTH_LONG).show();
+                Intent i = new Intent(getApplicationContext(), Inscription.class);
+                i.putExtra("Prénom", et_prenom.getText().toString());
+                i.putExtra("Nom", et_nom.getText().toString());
+                i.putExtra("Naissance", et_age.getText().toString());
+                i.putExtra("Taille", et_taille.getText().toString());
+                i.putExtra("Poids", et_poids.getText().toString());
+                i.putExtra("Objectif de poids", et_objectif.getText().toString());
+                startActivity(i);
             }
         });
     }
