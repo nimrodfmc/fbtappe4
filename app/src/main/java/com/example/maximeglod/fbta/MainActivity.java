93f75e8d9a695ea87a4c8f97c4e784a4f2023814
Jules Couraud <julescouraud@gmail.com>
2019-01-30 21:19:22 +0100

    Début consultation objectif calorie Ajout calories de tous les aliments

--- app/src/main/java/com/example/maximeglod/fbta/MainActivity.java
+++ app/src/main/java/com/example/maximeglod/fbta/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.maximeglod.fbta;
-
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //teste jules
+
+        Button inscription = (Button) findViewById(R.id.Button01);
+        inscription.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getBaseContext(), Inscription.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
+
+        Button alimentation = (Button) findViewById(R.id.Button02);
+        alimentation.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getBaseContext(), MainAliments.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
+
+        Button evolution = (Button) findViewById(R.id.Button03);
+        evolution.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getBaseContext(), Evolution.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
+
+        Button activite_sportive = (Button) findViewById(R.id.Button04);
+        activite_sportive.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getBaseContext(), ActiviteSportive.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
     }
 }
